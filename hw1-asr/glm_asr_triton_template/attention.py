8a5733d89c3ad21409d730eda82cb09bb3f1401a
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2026-03-10 22:15:59 +0000

    fix

--- hw1-asr/glm_asr_triton_template/attention.py
+++ hw1-asr/glm_asr_triton_template/attention.py
@@ -507,7 +507,7 @@ def scaled_dot_product_attention(
             output.stride(0), output.stride(1), output.stride(2),
             mask_tensor.stride(0), mask_tensor.stride(1), mask_tensor.stride(2),
             BLOCK_D=head_dim_padded,
-            BLOCK_Q=16, BLOCK_K=16, BLOCK_D=head_dim_padded,
+            BLOCK_Q=16, BLOCK_K=16,
             IS_CAUSAL=is_causal, HAS_MASK=HAS_MASK)
         # print(f"Flash attention best config: {flash_attention_kernel.best_config}")
         # scores_2d = scores.reshape(batch * num_heads * seq_q, seq_k_padded)

967d824ac009c378469f426edd123607f723406a
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2026-03-10 20:55:46 +0000

    unfuse

--- hw1-asr/glm_asr_triton_template/__init__.py
+++ hw1-asr/glm_asr_triton_template/__init__.py
@@ -17,8 +17,8 @@ if _dir not in sys.path:
 from . import layers
 
 layers.Linear.BACKEND = "triton"
-layers.MLP.FUSED = True
-layers.EncoderMLP.FUSED = True
+layers.MLP.FUSED = False
+layers.EncoderMLP.FUSED = False
 
 from . import model
 from . import rope
